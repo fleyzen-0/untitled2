872472c1f1f844ea28e8a08f25e01fce35356d40
“Your <yaroslavdisiak@gmail.com>
2022-10-08 11:15:43 +0300

    done hm_ex_2

---
+++ main.py
@@ -0,0 +1,12 @@
+try:
+    line = int(input("Введіть довжину лінії: "))
+    simvol = input("Введіть символ для заповнення: ")
+    for item in range(0, line):
+        print(simvol)
+
+
+
+except Exception as ex:
+    print("Erorr: ", ex)
+finally:
+    print(" Завдання виконано.")
