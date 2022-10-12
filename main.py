fed8b6971933d6cd9665c234f2bd3c18534102d3
fleyzen-0 <83985851+fleyzen-0@users.noreply.github.com>
2022-10-12 23:51:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 try:
-    chisla = int(input("Введіть число: "))
+    sum = 0
+    chisla = 0
+    while chisla != 7:
+        chisla = int(input("Введіть число: "))
+        if chisla == 7:
+
+            print("Сумма чисел -> ", sum)
+            break
+        else:
+            sum += chisla
+
 
 
 
